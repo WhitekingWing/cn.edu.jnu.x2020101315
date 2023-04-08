19462727f246c3863be627397e0ae92145a0f57c
WhitekingWing <189642429@qq.com>
2023-04-08 16:00:47 +0800

    third test

--- app/src/test/java/cn/edu/jnu/x2020101315/BowlingGameUnitTest.java
+++ app/src/test/java/cn/edu/jnu/x2020101315/BowlingGameUnitTest.java
@@ -1,5 +1,6 @@
 package cn.edu.jnu.x2020101315;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -10,13 +11,32 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class BowlingGameUnitTest {
+    private BowlingGame game;
+    @Before
+    public void init(){
+        game = new BowlingGame();
+    }
     @Test
     public void test_roll_first() {
-        BowlingGame game = new BowlingGame();
         for(int i = 0;i < 10;i++)
         {
             game.roll(1);
         }
         assertEquals(game.score, 1 * 10);
+    }
+    @Test
+    public void test_roll_second() {
+        game.roll(3);
+        game.roll(7);
+        game.roll(8);
+        rollMore(17,0);
+        assertEquals(game.score, 26);
+    }
+
+    private void rollMore(int n, int i) {
+        for(int j = 0;j < n;j++)
+        {
+            game.roll(i);
+        }
     }
 }
